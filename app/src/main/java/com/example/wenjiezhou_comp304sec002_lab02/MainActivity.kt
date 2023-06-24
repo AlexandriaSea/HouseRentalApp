4fc92eafb1cd98202b0ce79ecde1105a59c17f3e
Wenjie Zhou <jack-zwj@hotmail.com>
2023-06-23 20:19:27 -0400

    Clear the preference after main page

--- app/src/main/java/com/example/wenjiezhou_comp304sec002_lab02/MainActivity.kt
+++ app/src/main/java/com/example/wenjiezhou_comp304sec002_lab02/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.wenjiezhou_comp304sec002_lab02
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +13,11 @@ class MainActivity : AppCompatActivity() {
 
         val enter = findViewById<Button>(R.id.buttonEnter)
         enter.setOnClickListener{
+            val sharedPreferences = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.clear()
+            editor.apply()
+
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
